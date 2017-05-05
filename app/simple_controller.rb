b9c93f47c2738014f17fd01b4af3013504dcc14a
Mike Peritore <michael.peritore@americastestkitchen.com>
2017-05-05 12:09:27 -0400

    wip

--- app/simple_controller.rb
+++ app/simple_controller.rb
@@ -4,16 +4,38 @@ class App::SimpleController < App
 
   get '/' do
     sql = "SELECT title from recipes limit 50;"
-
     @recipes = ActiveRecord::Base.connection.select_all(sql)
-    puts @recipes.inspect
 
     render 'index.html.erb'
   end
   alias_endpoint '/index', '/'
 
-  get '/test' do
-    JSON.dump({"type" => "something"})
+  get '/payload' do
+    sql = "SELECT payload from document_payloads limit 1;"
+    record = ActiveRecord::Base.connection.select_one(sql)
+    @payload = JSON.parse(record["payload"])["recipe"]
+    puts @payload["recipe"]
+    # JSON.dump(@payload["recipe"])
+    render 'payload.html.erb'
   end
 
+  get '/recipe' do
+    sql = "SELECT payload from document_payloads where id=3;"
+    record = ActiveRecord::Base.connection.select_one(sql)
+    @payload = JSON.parse(record["payload"])["recipe"]
+    puts @payload["recipe"]
+    # JSON.dump(@payload["recipe"])
+    render 'payload.html.erb'
+  end
+
+  get '/recipes/:id' do
+    # where_clause = "meta @> {'document_id': #{params[:id].to_i}, \"'document_type': 'recipe', 'context': 'document'}\""
+    sql = "SELECT payload from document_payloads where meta @> '{\"document_id\": #{params['id'].to_i}, \"document_type\": \"recipe\", \"context\": \"document\"}'\;"
+    puts sql
+    record = ActiveRecord::Base.connection.select_one(sql)
+    @payload = JSON.parse(record["payload"])["recipe"]
+    puts @payload["recipe"]
+    # JSON.dump(@payload["recipe"])
+    render 'payload.html.erb'
+  end
 end
